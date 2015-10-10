bfd540eacb6ba72b962cfc4810f641d4d0fcacb5
Richard <!Theman315saville.rich@gmail.com>
2015-10-10 16:30:06 -0700

    add smooth follow and ui scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	static GameManager GM;
+	// Use this for initialization
+	IEnumerator Start () {
+		AsyncOperation async = Application.LoadLevelAdditiveAsync("UI_Scene");
+		yield return async;
+		Debug.Log("Loading complete");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
