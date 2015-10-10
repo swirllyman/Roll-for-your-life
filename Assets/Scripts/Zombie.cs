1161dfe40d1357e791c61f0149ca4d2f31e6f2fa
Joe Greive <swirllyman@gmail.com>
2015-10-10 16:34:42 -0700

    Added Zombie
    Added Zombie and zombie script

---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zombie : MonoBehaviour {
+
+	public float maxChoiceTimer = 5.0f;
+	public float choiceTimer;
+	Animation anim;
+
+	float speed = .02f;
+
+	bool chillin;
+	// Use this for initialization
+	void Start () {
+		choiceTimer = maxChoiceTimer;
+		anim = GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		choiceTimer -= Time.deltaTime;
+
+		if(choiceTimer <= 0.0f){
+			choiceTimer = maxChoiceTimer;
+			if(Random.value > .66f)
+			{
+				transform.Rotate (0, Random.Range (0, 360), 0);
+				chillin = true;
+				speed = 0;
+			}
+			else
+			{
+				chillin = false;
+				speed = .02f;
+			}
+		}
+
+		if(!chillin){
+			transform.Translate(Vector3.forward * speed);
+		}
+
+
+		if(chillin){
+			anim.CrossFade("idle02");
+		}
+		else
+		{
+			anim.CrossFade("walk03");
+		}
+	}
+}
