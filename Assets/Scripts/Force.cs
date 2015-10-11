9e1376894e3621f39a227746735e1fda3162d053
Trey <tgrunnagle@gmail.com>
2015-10-11 12:33:44 -0700

    Make bubble boy prefab faster

--- Assets/Scripts/Force.cs
+++ Assets/Scripts/Force.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Force : MonoBehaviour {
 
-	public float pushAmount = 1000;
+	public float pushAmount = 25000;
 	// Use this for initialization
 	void Start () {
 	
@@ -19,7 +19,7 @@ public class Force : MonoBehaviour {
 	{
 		if (GameManager.Active && c.tag == "Player") {
 			var dist = Vector3.Distance(c.gameObject.transform.position, this.transform.parent.position);
-			c.attachedRigidbody.AddForce(transform.forward * pushAmount/Mathf.Pow(dist, 2));
+			c.attachedRigidbody.AddForce(transform.forward * pushAmount/dist);
 		}
 	}
 }
