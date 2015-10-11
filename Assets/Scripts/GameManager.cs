642c3fe320af175ffa8084b0d98297f77e713549
Trey <tgrunnagle@gmail.com>
2015-10-11 13:41:14 -0700

    Add spawn point
    Add spawn point prefab. If it exists in a level, a player is
    instantiated there and is followed by the playcam.
    Using a spawn point ensures that the player has all the settings
    (defined by the prefab).

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,15 +6,34 @@ public class GameManager : MonoBehaviour {
 	private static Camera setupCam;
 	private static Camera playCam;
 
+	private GameObject player;
+
 	// indicates if the ball is rolling
 	public static bool Active = false;
 
 	// Use this for initialization
 	IEnumerator Start () {
+		// initialize cameras
 		setupCam = GameObject.Find ("Main Camera").GetComponent<Camera>();
 		playCam = GameObject.Find ("PlayCam").GetComponent<Camera>();
 		playCam.enabled = false;
 		playCam.GetComponent<AudioListener>().enabled = false;
+
+		// find the spawnpoint. if it exists create a player and put it there.
+		GameObject spawnPoint = GameObject.Find ("SpawnPoint");
+		if (null != spawnPoint) {
+			Transform spawnPointTransform = GameObject.Find ("SpawnPoint").GetComponent<Transform> ();
+			this.player = (GameObject)Instantiate(Resources.Load ("BubbleBoy"));
+			Rigidbody playerBody = player.GetComponent<Rigidbody> ();
+			playerBody.position = spawnPointTransform.position;
+
+			CamFollow follow = playCam.GetComponentInParent<CamFollow> ();
+			follow.target = this.player.transform;
+			follow.body = playerBody;
+		} else {
+			Debug.LogWarning ("Failed to get spawnpoint");
+		}
+
 		GameManager.Active = false;
 
 		AsyncOperation async = Application.LoadLevelAdditiveAsync("UI_Scene");
