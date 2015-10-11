f60d91902357a46b8d602a5aafb75d241a5369dd
Jason Varbedian <jpvarbed@outlook.com>
2015-10-10 18:24:34 -0700

    add teleporter

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+	public Teleporter buddyTeleporter;
+
+	public void SetBuddy()
+	{
+		isBuddy = true;
+	}
+	void OnTriggerEnter(Collider ball)
+	{
+		if (!this.isBuddy) {
+			buddyTeleporter.SetBuddy();
+			ball.transform.position = buddyTeleporter.transform.position;
+		}
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private bool isBuddy = false;
+}
