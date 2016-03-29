82048b85ee10c802831bc58b11c8b4366b1aa03a
swirllyman <swirllyman@gmail.com>
2016-03-29 14:04:27 -0700

    Level changes, and sound changes
    Sound now has multiple songs, you can parse through them as well

--- Assets/Scripts/Master.cs
+++ Assets/Scripts/Master.cs
@@ -5,10 +5,12 @@ public class Master : MonoBehaviour {
 
 	//Level number, highest score
 	public static int[,] scores = new int[35, 2];
+    public GameObject sound;
 
 	// Use this for initialization
 	void Start () {
 		DontDestroyOnLoad(this);
+        //Instantiate(sound);
 	}
 	
 	// Update is called once per frame
--- Assets/SoundLooper.cs
+++ Assets/SoundLooper.cs
@@ -3,18 +3,40 @@ using System.Collections;
 
 public class SoundLooper : MonoBehaviour {
 
+    public bool mute = false;
+
+    public AudioClip[] songs;
+    AudioSource aSource;
+    int currentSongNumber = 0;
+
 	// Use this for initialization
 	void Start () {
+        aSource = GetComponent<AudioSource>();
+        aSource.clip = songs[currentSongNumber];
+        currentSongNumber++;
+        aSource.Play();
 		DontDestroyOnLoad (gameObject);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        aSource.mute = mute;
+
+        if (!aSource.isPlaying) {
+            NextSong();
+        }
+
+    }
+
+
+    public void NextSong() {
+        aSource.clip = songs[currentSongNumber];
+        currentSongNumber = ((currentSongNumber + 1) % songs.Length);
+        aSource.Play();
+    }
 
 	public void ToggleSound()
 	{
-		GetComponent<AudioSource>().mute = !GetComponent<AudioSource>().mute;
+        mute = !mute;
 	}
 }
